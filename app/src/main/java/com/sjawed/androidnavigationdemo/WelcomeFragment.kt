11d3aeec7ab2cc575482f38a1d5fc5f92c95c012
Salman Jawed <sjawed@folio3.com>
2019-02-25 19:35:48 +0500

    Initial commit

---
+++ app/src/main/java/com/sjawed/androidnavigationdemo/WelcomeFragment.kt
@@ -0,0 +1,32 @@
+package com.sjawed.androidnavigationdemo
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+        },2000)
+    }
+}
